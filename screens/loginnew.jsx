aa0e133b3ed7b2a64bf6a771eb02d9cff1a5d249
m-atique <atiquenwl@gmail.com>
2024-02-09 15:03:15 +0500

    updated

--- screens/loginnew.jsx
+++ screens/loginnew.jsx
@@ -22,7 +22,9 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewLoginScreen';
 import LinearGradient from 'react-native-linear-gradient';
-import icon from 
+import AntDesignIcons from 'react-native-vector-icons/AntDesign'
+import Entypo  from 'react-native-vector-icons/Entypo'
+
 
 
 
@@ -34,12 +36,19 @@ function Login() {
   return (
    <View className =" realtive flex items-center  h-screen bg-slate-300 ">
 
-    <View className="relative  h-[30%] w-[110%] rounded-b-full border-r-[16px] border-r-yellow-400   ">
+    {/* <View className="relative  h-[30%] w-[110%] rounded-b-full border-r-[16px] border-r-yellow-400   ">
     <Image source={require('../public/nhmpclg.jpeg')}   className='w-full h-full rounded-b-full   flex opacity-40 bg-[#125975]' />
-    </View>
- <Image source={require('../public/logo.png')}   className=' absolute w-20 h-20 bg-blue-800 rounded-full top-[24%]' />
+    </View> */}
+ {/* <Image source={require('../public/logo.png')}   className=' absolute w-20 h-20 bg-blue-800 rounded-full top-[24%]' /> */}
 
-<View className="h-2/6  flex w-5/6 items-center justify-center mt-8">
+ <View className="relative  h-[46%] w-[110%]  ">
+ <Image source={require('../public/loginbck.png')}   className='h-[100%] w-[100%]    flex ' />
+ </View>
+
+
+<View className="h-1/4 flex w-5/6 items-center justify-center  mb-8 ">
+
+  <AntDesignIcons name ="" />
 <TextInput type ="text" className=" border-b-2 border-slate-800 w-full"  />
 
 <TextInput type ="text" className=" border-b-2 border-slate-800 w-full mt-5"  />
@@ -60,10 +69,8 @@ function Login() {
 </View>
 
 <View className='absolute   w-full bottom-5  '>
-<Text className='text-slate-900 text-center  text-md p-2 font-bold  font-sans'>All rights resereved by IT Wing T/C NHMP</Text> 
+<Text className='text-slate-900 text-center  text-md p-2 font-bold  font-sans'>All rights resereved by IT Wing Training College NHMP</Text> 
 </View>
-
-
 
    </View>
   );
